686611152811c8a0fe42d52a7bfbe3934becb06a
mahjabeensultana2012 <41849247+mahjabeensultana2012@users.noreply.github.com>
2018-12-13 11:39:02 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const Dragon = require('./dragon');
+
+const fooey = new Dragon({ birthdate: new Date(), nickname: 'fooey' });
+const baloo = new Dragon({
+  birthdate: new Date(),
+  nickname: 'baloo',
+  traits: [
+    {
+      traitType: 'backgroundColor',
+      traitValue: 'green',
+    },
+  ],
+});
+
+const mimer = new Dragon();
+
+setTimeout(() => {
+  const gooby = new Dragon();
+  console.log('gooby', gooby);
+}, 3000);
+
+console.log('fooey', fooey);
+console.log('baloo', baloo);
+console.log('mimer', mimer);
