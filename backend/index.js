bc0ee50178fe94fc999f06e8eb17fe655373996b
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2018-12-18 12:50:48 -0600

    generation

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,37 @@
-const Dragon = require('./dragon');
+const Generation = require('./generation');
+const generation = new Generation();
 
-const fooey = new Dragon({ birthdate: new Date(), nickname: 'fooey' });
-const baloo = new Dragon({
-  birthdate: new Date(),
-  nickname: 'baloo',
-  traits: [
-    {
-      traitType: 'backgroundColor',
-      traitValue: 'green',
-    },
-  ],
-});
+console.log('generation', generation);
 
-const mimer = new Dragon();
+const gooby = generation.newDragon();
+console.log('gooby', gooby);
 
 setTimeout(() => {
-  const gooby = new Dragon();
-  console.log('gooby', gooby);
-}, 3000);
+  const mimar = generation.newDragon();
+  console.log('mimar', mimar);
+}, 15000);
 
-console.log('fooey', fooey);
-console.log('baloo', baloo);
-console.log('mimer', mimer);
+// const Dragon = require('./dragon');
+
+// const fooey = new Dragon({ birthdate: new Date(), nickname: 'fooey' });
+// const baloo = new Dragon({
+//   birthdate: new Date(),
+//   nickname: 'baloo',
+//   traits: [
+//     {
+//       traitType: 'backgroundColor',
+//       traitValue: 'green',
+//     },
+//   ],
+// });
+
+// const mimer = new Dragon();
+
+// setTimeout(() => {
+//   const gooby = new Dragon();
+//   console.log('gooby', gooby);
+// }, 3000);
+
+// console.log('fooey', fooey);
+// console.log('baloo', baloo);
+// console.log('mimer', mimer);
